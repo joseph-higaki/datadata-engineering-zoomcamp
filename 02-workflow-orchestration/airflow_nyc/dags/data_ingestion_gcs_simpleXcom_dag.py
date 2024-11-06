5e6dd40826a4f9c55165d74b9eda1128d26ba6be
Joseph Higaki <josephhigaki@hotmail.com>
2024-11-06 14:48:43 +0100

    02 orch - nyc yellow works with dag and simple XCON

--- 02-workflow-orchestration/airflow_nyc/dags/data_ingestion_gcs_simpleXcom_dag.py
+++ 02-workflow-orchestration/airflow_nyc/dags/data_ingestion_gcs_simpleXcom_dag.py
@@ -15,45 +15,62 @@ PROJECT_ID = os.environ.get("GCP_PROJECT_ID")
 BUCKET_ID = os.environ.get("GCP_GCS_BUCKET")
 dataset_file = "yellow_tripdata_2021-01.csv.gz"
 source_dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/{dataset_file}"
+
+#source_dataset_url = f"https://d37ci6vzurychx.cloudfront.net/trip-data/{dataset_file}"
+#dataset_file = "yellow_tripdata_2021-01.parquet"
+
 path_to_local_home_storage = f"{os.environ.get("AIRFLOW_HOME", "/opt/airflow/")}/raw_data"
 path_temp_local_dataset_file = f"{path_to_local_home_storage}/{dataset_file}"
 dataset_parquetfile = lambda srcfile : srcfile.replace('.csv.gz', '.parquet')
 
 def hello():
-    logger = logging.getLogger("airflow.data_ingestion_gcs_dag")
+    logger = logging.getLogger("data_ingestion_gcs_simpleXcom_dag")
     logger.info("Starting  test")
     print(f"hello world{1/1}")
     logger.info("Finished test")
 
-def format_to_parquet(src_file):
-    logger = logging.getLogger("airflow.data_ingestion_gcs_dag")
-    logger.info("format_to_parquet")
-    if not (src_file.endswith('.csv') or src_file.endswith('.csv.gz')) :
-        logger.error(f"Unkown file format extension {src_file}")
-        return 1
+def format_to_parquet(full_path_src_file):
+    logger = logging.getLogger("data_ingestion_gcs_simpleXcom_dag")
+    logger.info(f"format_to_parquet: {full_path_src_file}")
+        
+    if not (full_path_src_file.endswith('.csv') or full_path_src_file.endswith('.csv.gz')) :
+        logger.error(f"Unkown file format extension {full_path_src_file}")        
     else:
-        table = pv.read_csv(src_file)
-        pq.write_table(table, dataset_parquetfile(src_file))
-        logger.info("parquet written") 
-        logger.info(f"removing csv file {src_file}")
-        os.remove(src_file)
-        return 0
+        table = pv.read_csv(full_path_src_file)
 
-def local_to_gcs(bucket_name: str, destination_blob_name: str, local_filename: str):
-    logger = logging.getLogger("airflow.data_ingestion_gcs_dag.hello_task")
+        # Just replace .csv.gz for .parquet
+        full_path_output_parquet_file = dataset_parquetfile(full_path_src_file)
+        pq.write_table(table, full_path_output_parquet_file)
+        logger.info(f"parquet written {full_path_output_parquet_file}")         
+        os.remove(full_path_src_file)
+        logger.info(f"removed csv file {full_path_src_file}")
+        return full_path_output_parquet_file
+
+def local_to_gcs(bucket_name: str, **kwargs):
+    logger = logging.getLogger("data_ingestion_gcs_simpleXcom_dag")
     logger.info("local_to_gcs")
+    #logger.info(f"kwargs: {kwargs}")    
+    #logger.info(f"ti: {kwargs['ti']}")
+    task_instance = kwargs['ti']
+
+    local_filename = task_instance.xcom_pull(task_ids="format_to_parquet_task", key="return_value")
+    logger.info(f"local_filename: {local_filename}")    
+
+    destination_blob_name = os.path.basename(local_filename)
+    logger.info(f"destination_blob_name: {destination_blob_name}")       
+
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(destination_blob_name)
     generation_match_precondition = 0
     blob.upload_from_filename(local_filename, if_generation_match=generation_match_precondition)
-    logger.info("uploaded succesfully")
-    logger.info(f"removing csv file {local_filename}")
+    logger.info(f"uploaded {local_filename} succesfully to {bucket_name}")    
     os.remove(local_filename)
-    return 0
+    logger.info(f"removed csv file {local_filename}")    
+    return f"gs://{bucket_name}/{destination_blob_name}"
 
 with DAG(
-    'data_ingestion_gcs',
+    'data_ingestion_gcs_simpleXcom',
     schedule=None,  # Manual trigger only
     start_date=datetime(2024, 1, 1),
     catchup=False,
@@ -68,24 +85,25 @@ with DAG(
 
     download_dataset_task = BashOperator(
         task_id="download_dataset_task",
+        #bash_command=f"echo {source_dataset_url} create-dirs output {path_temp_local_dataset_file}"        
         bash_command=f"curl -sSL {source_dataset_url} --create-dirs --output {path_temp_local_dataset_file}"
     )
 
     format_to_parquet_task = PythonOperator(
-        task_id="format_to_parquet_task",
-        python_callable=format_to_parquet,
+        task_id="format_to_parquet_task",        
+        python_callable=format_to_parquet,        
         op_kwargs={
-            "src_file":path_temp_local_dataset_file
+            "full_path_src_file":path_temp_local_dataset_file
         }
     )
 
     local_to_gcs_task = PythonOperator(
         task_id="local_to_gcs_task",
         python_callable=local_to_gcs,
+        #provide_context=True,  # Enable context passing
         op_kwargs={
             "bucket_name":BUCKET_ID,
-            "destination_blob_name": dataset_parquetfile(dataset_file),
-            "local_filename":dataset_parquetfile(path_temp_local_dataset_file)
+            "destination_blob_name": dataset_parquetfile(dataset_file)            
         }
     )
 
@@ -95,11 +113,11 @@ with DAG(
             "tableReference": {
                 "project_id": PROJECT_ID,
                 "datasetId": "dataset_1",
-                "tableId": "external_table"
+                "tableId": "external_table_xcom"
             },
             "externalDataConfiguration":{
                 "sourceFormat": "PARQUET",
-                "sourceUris": [f"gs://{BUCKET_ID}/{dataset_parquetfile(dataset_file)}"]
+                "sourceUris": ["{{ task_instance.xcom_pull(task_ids='local_to_gcs_task', key='return_value') }}"]
             }
         }
     )    
--- 02-workflow-orchestration/airflow_nyc/readme.md
+++ 02-workflow-orchestration/airflow_nyc/readme.md
@@ -2,8 +2,35 @@
 
 Downloaded the dataset DONE
 format it to parquet DONE
-pending: upload to GCS
-pending: upload to GCS -> without parquet disc
+pending: upload DONE
+pending: upload to GCS -> without parquet disc - PENDING
 ![alt text](../../_resources/02-workflow-orchestration/airflow_nyc/readme.md/image.png)
 
 docker compose -f airflow_nyc_docker-compose.yaml down
+
+## DAG with local storage
+
+![alt text](../../_resources/02-workflow-orchestration/airflow_nyc/readme.md/image-1.png)
+
+* Download Dataset: Downloads file and stores csv at Schedulers: `opt/airflowraw/data` folder 
+* Format to Parquet: Uses the same folder `opt/airflowraw/data`, re-formats to parquet and deletes csv
+* Local to GCS: Loads from  folder `opt/airflowraw/data` uploads to specified bucket, delets parquet
+* Upload to big query
+
+## DAG Task XCOM First simple storage
+Exchange file name between tasks
+
+```python
+    def format_to_parquet(full_path_src_file):
+        ...    
+        full_path_output_parquet_file = dataset_parquetfile(full_path_src_file)
+        ...
+        return full_path_output_parquet_file
+```
+XCom of task has a `return_value`:
+![alt text](../../_resources/02-workflow-orchestration/airflow_nyc/readme.md/image-2.png)
+
+
+
+## DAG Task XCOM Blob Backend
+exchange fillename, use GCS blob provider
