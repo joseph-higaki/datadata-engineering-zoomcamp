3ce4dbd1c64f486ed60615373acfe97a59355379
Joseph Higaki <joseph.higaki@gmail.com>
2024-11-15 17:08:03 +0000

    raw layer to standardize columns

--- 04-analytics-engineering/dbt_cloud/models/raw/raw_yellow_tripdata_2019_01.sql
+++ 04-analytics-engineering/dbt_cloud/models/raw/raw_yellow_tripdata_2019_01.sql
@@ -1,13 +1,12 @@
 with 
 source as (
     select * from {{ source('raw', 'yellow_tripdata_2019_01_external') }}
-    --select * from {{ source('staging', 'yellow_tripdata_external_2019_01') }}
-    --select * from {{ source('staging', 'green_tripdata_external') }}
+
 ),
 
 renamed as (
     select
-    --  get_monthly_table_name('yellow_tripdata_external_YYYY_MM',2019,01)  as table_name ,        
+    
     {% for item in ['2019_01','2019_02','2019_03','2019_04','2019_05','2019_06','2019_07','2019_08','2019_09','2019_10','2019_11','2019_12','2020_01','2020_02','2020_03','2020_04','2020_05','2020_06','2020_07','2020_08','2020_09','2020_10','2020_11','2020_12'] %}
         'yellow_tripdata_external_{{item }}' as yellow_tripdata_external_{{item }}
         {% if not loop.last %}
